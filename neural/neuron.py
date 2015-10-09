6fe96667b191123edc2c55862603efed4848e39e
Andreas Dørum <andreas.doerum@gmail.com>
2015-10-09 18:32:01 +0200

    add neural program

--- neural/neuron.py
+++ neural/neuron.py
@@ -3,16 +3,33 @@ import random
 __author__ = 'Andreas'
 
 
-class SimpleNeuron(object):
-    def __init__(self, input_length, weight_range):
-        self.weights = self.generate_weights(input_length, weight_range)
+def generate_weights(length, weight_range):
+    l = []
+    for i in range(length):
+        w = random.uniform(weight_range[0], weight_range[1])
+        l.append(w)
+    return l
 
-    def generate_weights(self, length, weight_range):
-        l = []
-        for i in range(length):
-            w = random.random() - weight_range
-            l.append(w)
-        return l
+
+class SimpleNeuron(object):
+    def __init__(self, input_length, weight_range, threshold_range, learning_rate):
+        self.learning_rate = learning_rate
+        self.weights = generate_weights(input_length, weight_range)
+        self.threshold = random.uniform(threshold_range[0], threshold_range[1])
 
     def fire(self, inputs):
-        pass
+        weight_sum = 0
+        for i in range(len(inputs)):
+            weight_sum += inputs[i]*self.weights[i]
+        if weight_sum > self.threshold:
+            return 1
+        else:
+            return 0
+
+    def activate(self, inputs, desired_output):
+        output = self.fire(inputs)
+        error = desired_output - output
+        # weight training
+        for i in range(len(self.weights)):
+            self.weights[i] += self.learning_rate * inputs[i] * error
+        return error
--- neural/neuron_program.py
+++ neural/neuron_program.py
@@ -1,8 +1,43 @@
+import neuron
+import sys
+
 __author__ = 'Andreas'
 
+
+# step 1: Initialize perceptron
+
+
 inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
-outputs = [0, 0, 0, 1]
+and_outputs = [0, 0, 0, 1]
+or_outputs = [0, 1, 1, 1]
 
-# create architecture
 
+def neural_network(desired_outputs):
+    perceptron = neuron.SimpleNeuron(2, (-0.5, 0.5), (-0.5, 0.5), 0.1)
+
+    epoch = 1
+    errors = 1
+    while errors and epoch < 30:
+
+        # step 2 and 3: activate and train
+        errors = 0
+        for p in range(len(inputs)):
+            e = perceptron.activate(inputs[p], desired_outputs[p])
+            if abs(e):
+                errors += 1
+
+        print("-------- After epoch {}: --------".format(epoch))
+        print("errors: {}"
+              " weight 1: {}"
+              " weight 2: {} ".format(errors, round(perceptron.weights[0],4), round(perceptron.weights[1], 6)))
+        epoch += 1
+
+for c in range(10):
+    print()
+    print("Goal: AND - cycle: ", c+1)
+    neural_network(and_outputs)
+
+for c in range(10):
+    print("Goal: OR - cycle: ", c+1)
+    neural_network(or_outputs)
 
